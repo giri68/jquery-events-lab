122e37c6961a93585670e14694b6cc3327510fcb
srijana thapa <giri.pdt@gmail.com>
2017-07-02 23:37:09 -0700

    jquery event assignment

--- starter-code/change/base.js
+++ starter-code/change/base.js
@@ -4,4 +4,9 @@ $(document).ready(function(){
 
   $("#time").text( Date.now() );
 
-})
+  $("input.add").on("change", function sum(event){
+  	var sum = parseInt($("input#left").val()) + parseInt($("input#right").val());
+  	$("#total").val(sum);
+  });
+
+});
--- starter-code/keypress/base.js
+++ starter-code/keypress/base.js
@@ -1,5 +1,28 @@
 console.log("Sanity Check: JS is working!");
+var startTime;
+var endTime;
 
 $(document).ready(function(){
 
+
+ $(window).on("keypress", function spacebar(event){
+
+    if ( startTime ) {
+      endTime = Date.now();
+      
+      totalTime();
+      
+    } else {
+      startTime = Date.now();
+     
+            
+    }
+
+  })
+
 })
+
+function totalTime(){
+  var total = (endTime - startTime) / 1000;
+  $("#total-time").text( total + " seconds" );
+}
